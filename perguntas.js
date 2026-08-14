6ef8a3c9552ef71c7421c9c2d8ac4b04fd0f94c7
nicolerodrigues19-stack <nicole.rodrigues19@escola.pr.gov.br>
2026-08-14 15:52:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Quem é',
+    'Quem é a Kuromi?',
+    'Uma personagem da turminha da Hello Kitty.'
+)
+
+criaCartao(
+    'Cor',
+    'Qual a cor principal da Kuromi?',
+    'Preto.'
+)
+
+criaCartao(
+    'Aparecimento',
+    'Quando a Kuromi apareceu pela primeira vez??',
+    'Ela apareceu no ano de 2005.'
+)
+
+criaCartao(
+    'Caveira',
+    'Qual a cor da caveira que Kuromi tem em seu capuz?',
+    'Rosa.'
+)
+
+criaCartao(
+    'Rival',
+    'Quem é a Rival da Kuromi?',
+    'My Melody'
+)
+
+criaCartao(
+    'Personalidade',
+    'Qual a personalidade da Kuromi?',
+    'Travessa, Divertida e Extrovertida'
+)
+
+criaCartao(
+    'Escrita',
+    'O que a Kuromi gosta de escrever?',
+    'Um diário.'
+)
+
+criaCartao(
+    'Animal Associado',
+    'Qual o animal é associado a Kuromi?',
+    'Um coelho.'
+)
+
+criaCartao(
+    ' Empressa ',
+    'Qual a empressa que a personagem Kuromi faz parte?',
+    'A Sanrio.'
+)
